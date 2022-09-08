fc3242595bff8684dd5d275d256624c0570ff723
hugotank99 <hugoramosjan@gmail.com>
2022-09-08 17:02:47 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,15 @@
+//criar uma variavel que receba o nome do meu usuario
+const userName = prompt ('type your name');
+const userHeight = prompt ('type your Height');
+const userAge = prompt ('type your age');
+const userDate = prompt ('type you date of admission');
+const userDiabetic = prompt ('type if you are diabetic')
+
+
+    console.log (`the name entered was ${userName}`);
+    console.log (`The height entered was ${userHeight}`);
+    console.log (`The age entered was ${userAge}`);
+    console.log (`The date of admission entered was ${userDate}`);
+    console.log (`typed diabetes was ${userDiabetic}`);
+    
+   
